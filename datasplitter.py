a4238b911fee0e65ade877515210dc76e682646a
PhilliesWorkLaptop <philipp.matten@gmx.de>
2023-06-24 15:47:07 +0200

    Added class in charge of splitting data

---
+++ datasplitter.py
@@ -0,0 +1,15 @@
+import random
+import pandas as pd
+
+class DataSplitter():
+    def __init__(self, frame) -> None:
+        self.in_frame = frame
+        
+    def return_percentage_split(self):
+        pass
+    
+    def return_fixed_num_per_category(self):
+        pass
+    
+    def return_random_mixed_split(self):
+        pass
